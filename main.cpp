b744b03b984f6c680972c61d490aa40bcea56ae4
Isaiah Sule <isaiah.sule001@mymdc.net>
2019-04-09 18:46:31 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,9 +14,8 @@ int MAX_WORD = 1;       //not const as file will change value
 
 //prototypes
 void displayMenu(int &);
-void populateVector(vector<string> & , int);
-string chooseSecretAnswer(vector<string>);
-void displayGame();
+void populateVector(vector<string>&, int);
+void chooseSecretAnswer(vector<string>);
 
 int main()
 {
@@ -33,24 +32,26 @@ int main()
 
     displayMenu(choice);
 
+    cout << "next..." << endl;
+
     if(choice == 6)
     {
         cout << "Thank you for playing our game" << endl;
         cout << "Have a good day." << endl;
         return 0;
     }
-
+    cout << "Safe code works..." << endl;
 
     populateVector(words,choice);
 
     for(int i = 0; i < words.size(); i++)
         cout << words.at(i) << endl;
 
-    char choices[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    //char choices[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
+    cout << "The vector size of vector word is: " << words.size() << endl;
+    chooseSecretAnswer(words);
 
-    answer = chooseSecretAnswer(words);
-    cout << answer;
 
     return 0;
 }
@@ -64,13 +65,15 @@ void displayMenu(int & choice)
     Parameters  :
     Returns     :
     */
-        cout << "Please choose from the following options:" << endl;
-        cout << "1. Animals" << endl;
-        cout << "2. Sports" << endl;
-        cout << "3. Cars" << endl;
-        cout << "4. Fruits and Vegetables" << endl;
-        cout << "5. Countries" << endl;
-        cout << "6. Exit" << endl;
+
+    cout << "inside displayMenu" << endl; //TESTING
+    cout << "Please choose from the following options:" << endl;
+    cout << "1. Animals" << endl;
+    cout << "2. Sports" << endl;
+    cout << "3. Cars" << endl;
+    cout << "4. Fruits and Vegetables" << endl;
+    cout << "5. Countries" << endl;
+    cout << "6. Exit" << endl;
      do
     {
         cout << "Your choice: ";
@@ -80,37 +83,45 @@ void displayMenu(int & choice)
 
     }while(cin.fail() || choice > 6 || choice < 1);
 
+    cout << "DONE..." << endl;
+
 }
-void populateVector(vector<string> &arr, const int choice) //choice should not be interfered with while in this function
+void populateVector(vector<string>&myV, const int choice) //choice should not be interfered with while in this function
 {
     /*
     Function    : populateVector
-    Programmer  : Isaiah Sule
-                  Eros Rodriguez
+    Programmer  : Isaiah Sule, Eros
     Date        : 4-9-2019
     Description : Opens a file based on the choice the user made from the last function
-    Parameters  : arr           (vector<string>) byReference
-                  choice        (const int) byValue
+    Parameters  : vector<string> & myV, const int choice
     Returns     :
     */
+    cout << "Inside populateVector" << endl;
+    cout << "-----------BEFORE---------------" << endl;
+    cout << "myV size is " << myV.size() << endl;
     ifstream iFile;
 
     switch(choice)
     {
     case 1:
         iFile.open("Animals.txt");
+        cout << "opened Animals.txt" << endl;
         break;
     case 2:
         iFile.open("Sports.txt");
+        cout << "opened Sports.txt" << endl;
         break;
     case 3:
         iFile.open("Cars.txt");
+        cout << "opened Cars.txt" << endl;
         break;
     case 4:
         iFile.open("Fruits and Vegetables.txt");
+        cout << "opened Fruits and Vegetables.txt" << endl;
         break;
     case 5:
         iFile.open("Countries.txt");
+        cout << "opened Countries.txt" << endl;
         break;
     default: //safe code
         cout << "Something went wrong. File not found." << endl;
@@ -119,43 +130,45 @@ void populateVector(vector<string> &arr, const int choice) //choice should not b
         break;
     }
 
+    cout << "Done with loop" << endl;
+
     //place data into the vector
-    string line; //stores data from each line of the file
-    while(!iFile.eof())
+    cout << "placing data into vector..." << endl;
+    string line = " "; //stores data from each line of the file
+    while(getline(iFile, line))
     {
-        getline(iFile,line);    //grabs characters in line
-        for(int i = 0; i < line.size(); i++) //loops through each character stored in line
-        {
-            line.at(i) = toupper(line.at(i)); //makes letter uppercase
-        }
-        arr.push_back(line); //stores line string in arr vector
+        cout << "inside loop" << endl;
+        myV.push_back(line);
+        cout << line << endl;
     }
+    cout << "DONE..." << endl;
+    cout << "-----------AFTER---------------" << endl;
+    cout << "myV size is " << myV.size() << endl;
 }
-string chooseSecretAnswer(vector<string> arr)
+void chooseSecretAnswer(vector<string> myV)
 {
     /*
     Function    : chooseSecretAnswer
     Programmer  : Isaiah Sule
     Date        : 4-9-2019
     Description : Creates an index out of a random number between 1 and the size of the vector.
-                  Stores the result in an integer, arrIndex.
-                  Declares secret answer using the stored index.
-    Parameters  : vector<string>arr     (string) byValue
-    Returns     : secretAnswer          (string)
+                 Stores the result in an integer, arrIndex.
+                 Declares secret answer using the stored index.
+    Parameters  : vector<string>myV
+    Returns     :
     */
+    cout << "<<<<<inside chooseSecretAnswer >>>>>>>>" << endl;
     string secretAnswer;
     int arrIndex;
+    arrIndex = rand() % (MAX_WORD - MIN_WORD + 1) + MIN_WORD; //this?
 
-    MAX_WORD = arr.size(); // between 1 and the max size of the vector
-    arrIndex = rand() % (MAX_WORD - MIN_WORD + 1) + MIN_WORD;
-    secretAnswer = arr.at(arrIndex);
+    cout << "the vector index is now " << arrIndex << endl;
+    cout << "the vector size is now" << myV.size() << endl;
+    secretAnswer = myV.at(arrIndex);
 
 
     cout << "The secret answer is " << secretAnswer << endl;
-    return secretAnswer;
 }
-
-
 
 /****************************************************************************
                         Professor-provided functions
